24f75d26c3536704a794e318081b7c0987526674
devmocharasa <devmocharasa@gmail.com>
2026-01-28 16:11:23 +0700

    added categories to layer architecture

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"kasir-api/database"
 	"kasir-api/handlers"
@@ -41,12 +42,21 @@ func main() {
 
 	//3. dependency injection [harus diatas HandleFunc]
 	productRepo := repositories.NewProductRepository(db)
+	categoryRepo := repositories.NewCategoryRepository(db)
 	productService := services.NewProductService(productRepo)
+	categoryService := services.NewCategoryService(categoryRepo)
 	productHandler := handlers.NewProductHandler(productService)
+	categoryHandler := handlers.NewCategoryHandler(categoryService)
 
 	//4. setup routes
+	//health check
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		healthCheck(w, r)
+	})
 	http.HandleFunc("/api/produk", productHandler.HandleProducts)
 	http.HandleFunc("/api/produk/", productHandler.HandleProductByID)
+	http.HandleFunc("/api/categories", categoryHandler.HandleCategories)
+	http.HandleFunc("/api/categories/", categoryHandler.HandleCategoryByID)
 
 	//5. run server
 	addr := "0.0.0.0:" + config.Port
@@ -56,4 +66,12 @@ func main() {
 		fmt.Println("gagal running server", err)
 	}
 
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"status":  "OK",
+		"message": "API running",
+	})
 }
